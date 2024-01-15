726f11b8bf7faaca824cef5b41c296b3a0284da7
JoanMilano <joanlester191@gmail.com>
2024-01-15 12:57:58 -0800

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -25,14 +25,14 @@ export const Banner = () => {
     const [isDeleting, setIsDeleting] = useState(false); // is word being deleted or not 
     const toRotate = [ "Web Dev. & Design", "E-commerce", "Digital Marketing", ]; // words to be rotate in the animation 
     const [text, setText] = useState(''); // continue-ally updated with each letter character as the animation types/deletes
-    const [delta, setDelta] = useState(300 - Math.random() * 100);  // the pace of typing (calculated randomly initially) by milliseconds
+    const [pace, setPace] = useState(300 - Math.random() * 100);  // the pace of typing (calculated randomly initially) by milliseconds
     const pause = 2000; // how much time it take for one string value to be typed out and deleted (full animation cycle)
 
 
     useEffect(() => { // function to create animation 
         let ticker = setInterval(() => { 
             tick(); // when tick is being fire off  
-            }, delta) // (setInterval value) time inbetween each Interval value
+            }, pace) // (setInterval value) time inbetween each Interval value
     
             return () => { clearInterval(ticker)};  // clean code and avoid memory leak
      
@@ -47,17 +47,17 @@ export const Banner = () => {
 
         setText(updatedText); // update state of text
 
-        if (isDeleting) { // if deleting then pace (delta) is faster 
-            setDelta(prevDelta => prevDelta / 2)
+        if (isDeleting) { // if deleting then pace (pace) is faster 
+            setPace(prevpace => prevpace / 2)
         }
 
         if (!isDeleting && updatedText === fullText) {
             setIsDeleting(true); 
-            setDelta(pause); // introduces 2000 ms pause b4 next stage
+            setPace(pause); // introduces 2000 ms pause b4 next stage
         } else if (isDeleting && updatedText === '') {
             setIsDeleting(false); 
             setLoopNum(loopNum + 1); // last step in the process in order to move on to next words index
-            setDelta(500); // introduces 500 ms pause b4 next stage
+            setPace(500); // introduces 500 ms pause b4 next stage
         }
     }
 
@@ -70,7 +70,7 @@ export const Banner = () => {
                     <h1>{'Hello I am jones'}</h1>
                     <h2 className="wrap">{text}</h2>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    <button><ArrowRightCircle size={25}/></button>
+                    <button>Let's connect<ArrowRightCircle size={25}/></button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                     <img src={headerImg} alt="Header Img" />
