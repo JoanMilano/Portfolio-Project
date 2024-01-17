740271c388e7f7a0d2e69ca5770087680efa4861
JoanMilano <joanlester191@gmail.com>
2024-01-17 15:10:34 -0800

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,11 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import Carousel from 'react-multi-carousel'; 
 import 'react-multi-carousel/lib/styles.css';
-import meter1 from '../assets/imgs/meter1.svg'; 
-import meter2 from '../assets/imgs/meter2.svg'; 
-import meter3 from '../assets/imgs/meter3.svg'; 
+import SEOImg from '../assets/imgs/SEO-Img.avif'; 
+import brandIdentityImg from '../assets/imgs/BrandIdentityImg.avif'; 
+import solMedManImg from '../assets/imgs/SolMedManImg.avif'; 
+import uiUxImg from '../assets/imgs/UI_UXImg.avif'; 
+import eComImg from '../assets/imgs/EComImg.avif'; 
 import colorSharp from '../assets/imgs/color-sharp.png'
 
 
@@ -37,23 +39,23 @@ export const Skills =  () => {
                <p>Starting out primarily as a UI/UX designer and developer aimed at building, customizing, and optimizing my clients online stores with a creative and comprehensive touch. I found interest in the skills surrounding digital marketing enabling me to give my client a round scope of the services they need for their business. Listed below are the skills I've become proficient in, and would be honored to gain experience with, while working on your masterpiece.</p>
                <Carousel responsive={responsive} infinite={true} className='skills-slider'>
                 <div className='item'>
-                    <img src={meter1} alt='Image' />
+                    <img src={SEOImg} alt='Image' />
                     <h5>SEO</h5>
                 </div>
                 <div className='item'>
-                    <img src={meter2} alt='Image' />
+                    <img src={brandIdentityImg} alt='Image' />
                     <h5>Brand identity</h5>
                 </div>
                 <div className='item'>
-                    <img src={meter3} alt='Image' />
+                    <img src={solMedManImg} alt='Image' />
                     <h5>Social Media Managment</h5>
                 </div>
                 <div className='item'>
-                    <img src={meter2} alt='Image' />
+                    <img src={uiUxImg} alt='Image' />
                     <h5>UI/UX Design & Development</h5>
                 </div>
                 <div className='item'>
-                    <img src={meter1} alt='Image' />
+                    <img src={eComImg} alt='Image' />
                     <h5>E-Commerce</h5>
                 </div>
                </Carousel>
