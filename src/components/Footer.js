36775858f9c84e6bf82a58ef950800218273bfa5
JoanMilano <joanlester191@gmail.com>
2024-01-22 19:12:39 -0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import navIcon1 from '../assets/imgs/nav-icon1.svg'
 import navIcon2 from '../assets/imgs/nav-icon2.svg'
 import navIcon3 from '../assets/imgs/nav-icon3.svg'
@@ -8,18 +9,20 @@ export const Footer = () => {
         <section className="footer">
               <span className="footer-icons">
                 <div className="social-icon">
-                    <a href="https://www.linkedin.com/in/joan-milano-3b541b29b/" target="_blank">  <img src={navIcon1} alt="linkedIn link"></img></a>
-                    <a href="https://github.com/JoanMilano" target="_blank">  <img src={navIcon2} alt="Github link"></img></a>
-                    <a href="https://www.instagram.com/jones_codes/" target="_blank">  <img src={navIcon3} alt="Instagram link"></img></a>
+                    <a href="https://www.linkedin.com/in/joan-milano-3b541b29b/" target="_blank" rel="noreferrer">  <img src={navIcon1} alt="linkedIn link"></img></a>
+                    <a href="https://github.com/JoanMilano" target="_blank" rel="noreferrer">  <img src={navIcon2} alt="Github link"></img></a>
+                    <a href="https://www.instagram.com/jones_codes/" target="_blank" rel="noreferrer">  <img src={navIcon3} alt="Instagram link"></img></a>
                 </div>
                 </span>
                 <div className='footer-text'>
                 <p>Thank you for your business</p>
                 <p>Produced with React, Bootstrap, & Express</p>
+                <p>Tested with jest, ___, and ___</p>
+                <p>Checkout my github for the full skillstack</p>
                 <p>Email: joan.milano829@gmail.com</p>
                 <p>Phone: (530)-313-0357</p>
                 </div>
-                <img src={footerImg}></img>
+                <img src={footerImg} alt="footer-design-img" ></img>
         </section>
     )
 }
