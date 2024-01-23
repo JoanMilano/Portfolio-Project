160e652a12abe37c96777937a6238ced91872c89
JoanMilano <joanlester191@gmail.com>
2024-01-22 19:12:29 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
@@ -11,7 +11,7 @@ import { Footer } from './components/Footer';
 function App() {
   return (
     <>
-    <div className="App">
+    <div data-testid="app-component">
       <NavBar />
       <Banner />
       <Skills />
