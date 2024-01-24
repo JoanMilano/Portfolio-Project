2a5820ea69b785cc3adaf747e53188edb3401c16
JoanMilano <joanlester191@gmail.com>
2024-01-23 19:46:36 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
-import { Skills } from "./components/Skills";
+import { Capabilities } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from './components/Footer';
@@ -14,7 +14,7 @@ function App() {
     <div data-testid="app-component">
       <NavBar />
       <Banner />
-      <Skills />
+      <Capabilities />
       <Projects />
       <Contact />
       <Footer />
